3de312c570daa681806df13eeded38bf9fc99c1a
BabintsevaS <sb.babintseva@gmail.com>
2021-05-20 20:11:41 -0700

    Formatting outputs have been added to class Main.java

--- src/Main.java
+++ src/Main.java
@@ -4,18 +4,21 @@ public class Main {
         double weight = 65.2;                     // вес в килограммах
         double height = 1.70;                    // рост в метрах
         double bodyIndex = service.calculate(weight, height);
-        System.out.println("Ваш индекс массы тела: " + bodyIndex);
+        System.out.printf("Ваш индекс массы тела: %.2f", bodyIndex);
+        System.out.println();
 
         BmiService service1 = new BmiService();
         double weight1 = 106.6;
         double height1 = 1.63;
         double bodyIndex1 = service1.calculate(weight1, height1);
-        System.out.println("Ваш индекс массы тела: " + bodyIndex1);
+        System.out.printf("Ваш индекс массы тела: %.2f", bodyIndex1);
+        System.out.println();
 
         BmiService service2 = new BmiService();
         double weight2 = 48.3;
         double height2 = 1.55;
         double bodyIndex2 = service2.calculate(weight2, height2);
-        System.out.println("Ваш индекс массы тела: " + bodyIndex2);
+        System.out.printf("Ваш индекс массы тела: %.2f", bodyIndex2);
+        System.out.println();
     }
 }
