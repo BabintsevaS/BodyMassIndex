5f5a2ce9e21775c68223513d2b08b09004af6974
BabintsevaS <sb.babintseva@gmail.com>
2021-05-21 09:28:08 -0700

    Improvements added

--- src/Main.java
+++ src/Main.java
@@ -1,23 +1,15 @@
 public class Main {
     public static void main(String[] args) {
         BmiService service = new BmiService();
-        double weight = 65.2;                     // вес в килограммах
-        double height = 1.70;                    // рост в метрах
-        double bodyIndex = service.calculate(weight, height);
+        double bodyIndex = service.calculate(65.2, 1.70); // вес в килограммах, рост в метрах
         System.out.printf("Ваш индекс массы тела: %.2f", bodyIndex);
         System.out.println();
 
-        BmiService service1 = new BmiService();
-        double weight1 = 106.6;
-        double height1 = 1.63;
-        double bodyIndex1 = service1.calculate(weight1, height1);
+        double bodyIndex1 = service.calculate(106.6, 1.63);
         System.out.printf("Ваш индекс массы тела: %.2f", bodyIndex1);
         System.out.println();
 
-        BmiService service2 = new BmiService();
-        double weight2 = 48.3;
-        double height2 = 1.55;
-        double bodyIndex2 = service2.calculate(weight2, height2);
+        double bodyIndex2 = service.calculate(48.3, 1.55);
         System.out.printf("Ваш индекс массы тела: %.2f", bodyIndex2);
         System.out.println();
     }
